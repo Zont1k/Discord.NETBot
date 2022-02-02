2587e030d5c5e7e6f83b11174b5dd7ce87df084d
Alexey Zotov <alekseyzotov2001@gmail.com>
2022-02-02 22:51:09 +0200

    Add project files.

---
+++ MooniBot/Services/StartupService.cs
@@ -0,0 +1,44 @@
+﻿using Discord.WebSocket;
+using System;
+using Discord.Commands;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Discord;
+using System.Reflection;
+
+namespace MooniBot.Services
+{
+    public class StartupService
+    {
+        public static IServiceProvider? _provider;
+        private readonly DiscordSocketClient _discord;
+        private readonly CommandService _commands;
+        private readonly IConfigurationRoot _config;
+
+        public StartupService(IServiceProvider provider, DiscordSocketClient discord, CommandService commands, IConfigurationRoot config)
+        {
+            _provider = provider;
+            _config = config;
+            _discord = discord;
+            _commands = commands;
+        }
+
+        public async Task StartAsync()
+        {
+            string token = _config["tokens:discord"];
+            if (string.IsNullOrEmpty(token))
+            {
+                Console.WriteLine("Please provide ypur Discord token in _config.yml");
+                return;
+            }
+
+            await _discord.LoginAsync(TokenType.Bot, token);
+            await _discord.StartAsync();
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _provider);
+        }
+    }
+}
